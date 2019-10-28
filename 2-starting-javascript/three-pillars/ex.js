7d93b593db79906867824d6b19a4d90ffeceb5d1
BenjaminJaume <benjamin.jaume@gmail.com>
2019-10-28 19:55:53 +1100

    Finishing exercises

--- 2-starting-javascript/three-pillars/ex.js
+++ 2-starting-javascript/three-pillars/ex.js
@@ -1,43 +1,51 @@
 class Bookshelf {
-	constructor() {
-		this.favoriteBooks = [];
-	}
+  constructor() {
+    this.favoriteBooks = [];
+  }
 
-	// TODO: define methods `addFavoriteBook(..)`
-	// and `printFavoriteBooks()`
+  // TODO: define methods `addFavoriteBook(..)`
+  // and `printFavoriteBooks()`
+
+  addFavoriteBook(bookName) {
+    if (!bookName.includes('Great')) {
+      this.favoriteBooks.push(bookName);
+    }
+  }
+
+  printFavoriteBooks() {
+    console.log(`Favorite Books: ${String(this.favoriteBooks.length)}`);
+    for (let bookName of this.favoriteBooks) {
+      console.log(bookName);
+    }
+  }
 }
 
-function addFavoriteBook(bookName) {
-	if (!bookName.includes("Great")) {
-		favoriteBooks.push(bookName);
-	}
+function loadBooks(bookshelf) {
+  // TODO: call fakeAjax( .. );
+  return fakeAjax(BOOK_API, function(booksArray) {
+    for (let book of booksArray) {
+      bookshelf.addFavoriteBook(book);
+    }
+    bookshelf.printFavoriteBooks();
+  });
 }
 
-function printFavoriteBooks() {
-	console.log(`Favorite Books: ${favoriteBooks.length}`);
-	for (let bookName of favoriteBooks) {
-		console.log(bookName);
-	}
-}
+var BOOK_API = 'https://some.url/api';
 
-function loadBooks( /* .. */ ) {
-	// TODO: call fakeAjax( .. );
-}
-
-var BOOK_API = "https://some.url/api";
-
+var instanceBookshelf = new Bookshelf();
+loadBooks(instanceBookshelf);
 
 // ***********************
 
 // NOTE: don't modify this function at all
-function fakeAjax(url,cb) {
-	setTimeout(function fakeLoadingDelay(){
-		cb([
-			"A Song of Ice and Fire",
-			"The Great Gatsby",
-			"Crime & Punishment",
-			"Great Expectations",
-			"You Don't Know JS"
-		]);
-	},500);
+function fakeAjax(url, cb) {
+  setTimeout(function fakeLoadingDelay() {
+    cb([
+      'A Song of Ice and Fire',
+      'The Great Gatsby',
+      'Crime & Punishment',
+      'Great Expectations',
+      "You Don't Know JS"
+    ]);
+  }, 500);
 }
--- dist/ex.22032b58.js
+++ dist/ex.22032b58.js
@@ -120,58 +120,100 @@ parcelRequire = (function (modules, cache, entry, globalName) {
 })({"2-starting-javascript/three-pillars/ex.js":[function(require,module,exports) {
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
-var Bookshelf = function Bookshelf() {
-  _classCallCheck(this, Bookshelf);
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
 
-  this.favoriteBooks = [];
-} // TODO: define methods `addFavoriteBook(..)`
-// and `printFavoriteBooks()`
-;
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
 
-function addFavoriteBook(bookName) {
-  if (!bookName.includes("Great")) {
-    favoriteBooks.push(bookName);
-  }
-}
+var Bookshelf =
+/*#__PURE__*/
+function () {
+  function Bookshelf() {
+    _classCallCheck(this, Bookshelf);
 
-function printFavoriteBooks() {
-  console.log("Favorite Books: ".concat(favoriteBooks.length));
-  var _iteratorNormalCompletion = true;
-  var _didIteratorError = false;
-  var _iteratorError = undefined;
+    this.favoriteBooks = [];
+  } // TODO: define methods `addFavoriteBook(..)`
+  // and `printFavoriteBooks()`
 
-  try {
-    for (var _iterator = favoriteBooks[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
-      var bookName = _step.value;
-      console.log(bookName);
+
+  _createClass(Bookshelf, [{
+    key: "addFavoriteBook",
+    value: function addFavoriteBook(bookName) {
+      if (!bookName.includes('Great')) {
+        this.favoriteBooks.push(bookName);
+      }
     }
-  } catch (err) {
-    _didIteratorError = true;
-    _iteratorError = err;
-  } finally {
+  }, {
+    key: "printFavoriteBooks",
+    value: function printFavoriteBooks() {
+      console.log("Favorite Books: ".concat(String(this.favoriteBooks.length)));
+      var _iteratorNormalCompletion = true;
+      var _didIteratorError = false;
+      var _iteratorError = undefined;
+
+      try {
+        for (var _iterator = this.favoriteBooks[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
+          var bookName = _step.value;
+          console.log(bookName);
+        }
+      } catch (err) {
+        _didIteratorError = true;
+        _iteratorError = err;
+      } finally {
+        try {
+          if (!_iteratorNormalCompletion && _iterator.return != null) {
+            _iterator.return();
+          }
+        } finally {
+          if (_didIteratorError) {
+            throw _iteratorError;
+          }
+        }
+      }
+    }
+  }]);
+
+  return Bookshelf;
+}();
+
+function loadBooks(bookshelf) {
+  // TODO: call fakeAjax( .. );
+  return fakeAjax(BOOK_API, function (booksArray) {
+    var _iteratorNormalCompletion2 = true;
+    var _didIteratorError2 = false;
+    var _iteratorError2 = undefined;
+
     try {
-      if (!_iteratorNormalCompletion && _iterator.return != null) {
-        _iterator.return();
+      for (var _iterator2 = booksArray[Symbol.iterator](), _step2; !(_iteratorNormalCompletion2 = (_step2 = _iterator2.next()).done); _iteratorNormalCompletion2 = true) {
+        var book = _step2.value;
+        bookshelf.addFavoriteBook(book);
       }
+    } catch (err) {
+      _didIteratorError2 = true;
+      _iteratorError2 = err;
     } finally {
-      if (_didIteratorError) {
-        throw _iteratorError;
+      try {
+        if (!_iteratorNormalCompletion2 && _iterator2.return != null) {
+          _iterator2.return();
+        }
+      } finally {
+        if (_didIteratorError2) {
+          throw _iteratorError2;
+        }
       }
     }
-  }
+
+    bookshelf.printFavoriteBooks();
+  });
 }
 
-function loadBooks()
-/* .. */
-{// TODO: call fakeAjax( .. );
-}
-
-var BOOK_API = "https://some.url/api"; // ***********************
+var BOOK_API = 'https://some.url/api';
+var instanceBookshelf = new Bookshelf();
+loadBooks(instanceBookshelf); // ***********************
 // NOTE: don't modify this function at all
 
 function fakeAjax(url, cb) {
   setTimeout(function fakeLoadingDelay() {
-    cb(["A Song of Ice and Fire", "The Great Gatsby", "Crime & Punishment", "Great Expectations", "You Don't Know JS"]);
+    cb(['A Song of Ice and Fire', 'The Great Gatsby', 'Crime & Punishment', 'Great Expectations', "You Don't Know JS"]);
   }, 500);
 }
 },{}],"../../../Users/Benjamin/AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
