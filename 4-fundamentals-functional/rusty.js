19533a74e25dc41c8962ea8045f04b17e8c4520a
BenjaminJaume <benjamin.jaume@gmail.com>
2019-11-17 10:58:04 +1100

    Continuing fourth course

---
+++ 4-fundamentals-functional/rusty.js
@@ -0,0 +1,16 @@
+// What the hell is that?
+
+// const { characterName, room, weapon } = {
+//   characterName: "Rusty",
+//   room: "kitchen",
+//   weapon: "candlestick"
+// };
+
+// This solution is better
+var obj = {
+  characterName: "Rusty",
+  room: "kitchen",
+  weapon: "candlestick"
+};
+
+const { characterName, room, weapon } = obj;
