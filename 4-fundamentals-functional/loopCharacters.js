081b83334874f4f37b067f22a2d957f6642d622e
BenjaminJaume <benjamin.jaume@gmail.com>
2019-11-17 12:17:48 +1100

    Updating loopCharacters

--- 4-fundamentals-functional/loopCharacters.js
+++ 4-fundamentals-functional/loopCharacters.js
@@ -2,20 +2,52 @@ const game = {
   suspects: [
     {
       name: "Rusty",
-      color: "orange"
+      color: "orange",
+      guilty: false
     },
     {
-      name: "Miss Scarlet",
-      color: "red"
+      name: "Mrs Scarlet",
+      color: "red",
+      guilty: false
     }
   ]
 };
 
-function getSuspects({ suspects }) {
+const guilty = "Mrs Scarlet";
+
+function displayCharacters(suspects) {
   suspects.forEach(character => {
-    // console.log(`Name: ${character.name}, color: ${character.color}`);
-    console.log(character);
+    console.log(`Name: ${character.name}, color: ${character.color}`);
   });
 }
 
-getSuspects(game);
+function findGuilty(suspects) {
+  suspects.forEach(character => {
+    if (character.name === guilty) {
+      character.guilty = true;
+    }
+  });
+}
+
+function displayGuilty(suspects) {
+  suspects.forEach(character => {
+    if (character.guilty === true) {
+      console.log(`${character.name} is guilty!`);
+    }
+  });
+}
+
+function getColor(suspects) {
+  let colors = [];
+  suspects.forEach(character => {
+    colors.push(character.color);
+  });
+  colors.forEach(color => {
+    console.log(color);
+  });
+}
+
+// displayCharacters(game.suspects);
+// findGuilty(game.suspects);
+// displayGuilty(game.suspects);
+getColor(game.suspects);
