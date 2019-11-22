23e9286c7a12d49f137a244618c23d5f5391e1a9
BenjaminJaume <benjamin.jaume@gmail.com>
2019-11-22 18:52:15 +1100

    Creating _.map

--- 4-fundamentals-functional/_.map.js
+++ 4-fundamentals-functional/_.map.js
@@ -1,7 +1,38 @@
-const weapons = ["candlestick", "lead pipe", "revolver"];
+function createSuspectObj(name) {
+  return {
+    name: name,
+    color: name.split(" ")[1],
+    speak() {
+      console.log(`My name is ${name}`);
+    }
+  };
+}
 
-const makeBroken = function(item) {
-  return `Broken ${item}`;
+var suspects = ["Miss Scarlet", "Colonel Mustard", "Mr. White", "Dr Magenta"];
+
+const _ = {};
+
+_.map = function(list, callback) {
+  // check if it's an array or an object
+  if (Array.isArray(list)) {
+    // Create an empty array to store
+    var storage = [];
+    // Looping
+    for (let i = 0; i < list.length; i++) {
+      // Callback(element) & p it to our array
+      storage.push(callback(list[i], i, list));
+    }
+    // Return []
+    return storage;
+  } else if (typeof list === "object") {
+    // something
+  } else {
+    console.log("Wrong type");
+  }
 };
 
-// const brokenWeapons.map()
+var suspectsList = _.map(suspects, function(suspect) {
+  return createSuspectObj(suspect);
+});
+
+suspectsList;
