e75795cf8075f46a63f7045de37d131110720a98
BenjaminJaume <benjamin.jaume@gmail.com>
2019-12-30 15:39:37 -0600

    adding npm prettier eslint

--- 5-intro-react/adopt-me/src/app.js
+++ 5-intro-react/adopt-me/src/app.js
@@ -1,17 +1,29 @@
-const Pet = () => {
+const Pet = ({ name, animal, breed }) => {
   return React.createElement("div", {}, [
-    React.createElement("h1", {}, "Bertrand"),
-    React.createElement("h2", {}, "Dog"),
-    React.createElement("h2", {}, "Cavoodle")
+    React.createElement("h1", {}, name),
+    React.createElement("h2", {}, animal),
+    React.createElement("h2", {}, breed)
   ]);
 };
 
 const App = () => {
   return React.createElement("div", {}, [
     React.createElement("h1", {}, "Adopt Me!"),
-    React.createElement(Pet),
-    React.createElement(Pet),
-    React.createElement(Pet)
+    React.createElement(Pet, {
+      name: "Baboon ",
+      animal: "dog",
+      breed: "Freud"
+    }),
+    React.createElement(Pet, {
+      name: "Bertrand ",
+      animal: "magpie",
+      breed: "millenial"
+    }),
+    React.createElement(Pet, {
+      name: "Beatrice ",
+      animal: "parrot",
+      breed: "furet"
+    })
   ]);
 };
 
