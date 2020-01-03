924bd33f4e6c051555983de6f9c9aa62aea6fe9d
BenjaminJaume <benjamin.jaume@gmail.com>
2020-01-03 09:21:23 -0600

    create component

--- 5-intro-react/adopt-me/src/app.js
+++ 5-intro-react/adopt-me/src/app.js
@@ -1,26 +1,19 @@
 import React from "react";
 import { render } from "react-dom";
-import Pet from "./Pet";
+// import Pet from "./Pet";
+import SearchParams from "./SearchParams";
 
 const App = () => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, "Adopt Me!"),
-    React.createElement(Pet, {
-      name: "Baboon ",
-      animal: "dog",
-      breed: "Freud"
-    }),
-    React.createElement(Pet, {
-      name: "Bertrand ",
-      animal: "magpie",
-      breed: "millenial"
-    }),
-    React.createElement(Pet, {
-      name: "Beatrice ",
-      animal: "parrot",
-      breed: "furet"
-    })
-  ]);
+  return (
+    <div>
+      <h1>Adopt Me!</h1>
+      {/* <Pet name="Baboon" animal="cat" breed="Freud" />
+      <Pet name="Bertrand" animal="magpie" breed="millenial" />
+      <Pet name="Beatrice" animal="parrot" breed="furet" /> */}
+
+      <SearchParams />
+    </div>
+  );
 };
 
-render(React.createElement(App), document.getElementById("root"));
+render(<App />, document.getElementById("root"));
