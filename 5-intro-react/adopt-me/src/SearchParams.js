1fd865c7161bc288ed9a48409e7f286cced16dc6
BenjaminJaume <benjamin.jaume@gmail.com>
2020-01-03 10:43:03 -0600

    async calls to Petfinder API, update pet component

--- 5-intro-react/adopt-me/src/SearchParams.js
+++ 5-intro-react/adopt-me/src/SearchParams.js
@@ -1,13 +1,25 @@
 import React, { useState, useEffect } from "react";
 import pet, { ANIMALS } from "@frontendmasters/pet";
+import Results from "./Results";
 import useDropdown from "./useDropdown";
 
 const SearchParams = () => {
-  // This is a Hook - all hooks begins with "use"
-  const [location, setLocation] = useState("Seattle, WA");
-  const [breeds, setBreeds] = useState([]);
-  const [animal, AnimalDropdown] = useDropdown("Animal", "dog", ANIMALS);
-  const [breed, BreedDropdown, setBreed] = useDropdown("Breed", "", breeds);
+  // These are hooks - all hooks begins with "use"
+  const [location, setLocation] = useState("Seattle, WA"); // React hook
+  const [breeds, setBreeds] = useState([]); // React hook
+  const [animal, AnimalDropdown] = useDropdown("Animal", "dog", ANIMALS); // Custom hook
+  const [breed, BreedDropdown, setBreed] = useDropdown("Breed", "", breeds); // Custom hook
+  const [pets, setPets] = useState("");
+
+  async function requestPets() {
+    const { animals } = await pet.animals({
+      location,
+      breed,
+      type: animal
+    });
+
+    setPets(animals || []);
+  }
 
   useEffect(() => {
     setBreeds([]);
@@ -21,7 +33,12 @@ const SearchParams = () => {
 
   return (
     <div className="search-params">
-      <form>
+      <form
+        onSubmit={event => {
+          event.preventDefault();
+          requestPets();
+        }}
+      >
         <label htmlFor="location">
           Location
           <input
@@ -36,6 +53,7 @@ const SearchParams = () => {
         <BreedDropdown />
         <button>Submit</button>
       </form>
+      <Results pets={pets} />
     </div>
   );
 };
--- 5-intro-react/adopt-me/src/app.js
+++ 5-intro-react/adopt-me/src/app.js
@@ -1,18 +1,16 @@
 import React from "react";
 import { render } from "react-dom";
-// import Pet from "./Pet";
 import SearchParams from "./SearchParams";
 
 const App = () => {
   return (
-    <div>
-      <h1>Adopt Me!</h1>
-      {/* <Pet name="Baboon" animal="cat" breed="Freud" />
-      <Pet name="Bertrand" animal="magpie" breed="millenial" />
-      <Pet name="Beatrice" animal="parrot" breed="furet" /> */}
+    <React.StrictMode>
+      <div>
+        <h1>Adopt Me!</h1>
 
-      <SearchParams />
-    </div>
+        <SearchParams />
+      </div>
+    </React.StrictMode>
   );
 };
 
